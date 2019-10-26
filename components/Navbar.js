13035d40ab5fcd72a9bdf69af6b449cd45002315
Tara Trody <shantara.trody@gmail.com>
2019-10-25 23:39:54 -0400

    complete desktop navigation

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-// import Link from 'next/link';
+import Link from 'next/link';
 import styled from 'styled-components';
 
 const Navbar = () => {
@@ -6,12 +6,34 @@ const Navbar = () => {
     <NavBar>
       <FlexContainer>
         <Logo>Group Fitness Instructors</Logo>
+
+        <NavLinks>
+          <Link href="https://google.com">
+            <NavItem>Join as Instructor</NavItem>
+          </Link>
+
+          <Link href="/">
+            <NavItem>FAQ</NavItem>
+          </Link>
+
+          <Link href="/">
+            <NavItem>Log In</NavItem>
+          </Link>
+
+          <Link href="/">
+            <Button>Sign up</Button>
+          </Link>
+        </NavLinks>
       </FlexContainer>
     </NavBar>
   );
 };
 
 export default Navbar;
+
+const mainText = ({ theme }) => theme.text.main;
+const primaryColor = ({ theme }) => theme.primary.main;
+const contrastText = ({ theme }) => theme.text.contrast;
 
 const NavBar = styled.header`
   width: 100%;
@@ -25,13 +47,35 @@ const FlexContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 0 1rem;
+  padding: 0 2rem;
   height: 5rem;
 `;
 
 const Logo = styled.a`
   font-weight: 900;
   font-style: italic;
-  color: ${({ theme }) => theme.primary.main};
+  color: ${primaryColor};
   font-size: 1.5rem;
+`;
+const Button = styled.a`
+  color: ${contrastText};
+  background-color: ${primaryColor};
+  padding: 0.5rem 1.5rem;
+  border-radius: 2px;
+  box-shadow: 0 2px 4px 0 rgba(5, 7, 15, 0.2);
+  text-transform: uppercase;
+  margin-right: 1rem;
+`;
+const NavLinks = styled.div`
+  display: flex;
+  padding: 1.5rem;
+  align-items: center;
+`;
+const NavItem = styled.a`
+  padding: 1rem;
+  color: ${mainText};
+
+  &:hover {
+    color: ${primaryColor};
+  }
 `;
--- components/SharedStyles.js
+++
@@ -1,9 +0,0 @@
-const styleToolbar = {
-  backgroundColor: '#FFF',
-  height: '78px',
-  paddingRight: '20px',
-};
-
-module.exports = {
-  styleToolbar,
-};
