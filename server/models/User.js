76e3e671be07ceb27ea4413e563a3f8526c54b67
Tara Trody <shantara.trody@gmail.com>
2019-10-29 21:25:03 -0400

    define user schema

---
+++ server/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  googleId: {
+    type: String,
+    required: false,
+    unique: true,
+  },
+  googleToken: {
+    access_token: String,
+    refresh_token: String,
+    token_type: String,
+    expiry_date: Number,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    require: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  displayName: String,
+  avatarUrl: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
