6a9322c38e477e9ef0192a4112b116acc676d806
Tara Trody <shantara.trody@gmail.com>
2019-10-31 21:25:57 -0400

    change to class component & wrap export with HOC

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,30 @@
+/* eslint-disable react/prefer-stateless-function */
 /* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
 import Head from 'next/head';
 import propTypes from 'prop-types';
 
-const Index = ({ user }) => (
-  <div style={{ padding: '10px 45px', marginTop: '100px' }}>
-    <Head>
-      <title>Index page</title>
-      <meta name="description" content="This is a description of the Index page" />
-    </Head>
+import withAuth from '../lib/withAuth';
 
-    <p>Email: {user.email}</p>
-  </div>
-);
-
-Index.getInitialProps = async (ctx) => ({ user: ctx.query.user });
+class Index extends React.Component {
+  render() {
+    const { user } = this.props;
+    return (
+      <div style={{ padding: '10px 45px', marginTop: '100px' }}>
+        <Head>
+          <title>Index page</title>
+          <meta name="description" content="This is a description of the Index page" />
+        </Head>
+        <p>Dashboard</p>
+        <p>Email: {user.email}</p>
+      </div>
+    );
+  }
+}
 
 Index.propTypes = {
   user: propTypes.shape({
-    email: propTypes.string.isRequired,
+    email: propTypes.string,
   }),
 };
 
@@ -25,4 +32,4 @@ Index.defaultProps = {
   user: null,
 };
 
-export default Index;
+export default withAuth(Index);
