34814038f3b1e386766f69127283fa666ec5e17c
Tara Trody <shantara.trody@gmail.com>
2019-11-23 22:58:40 -0500

    implement passport local strategy

---
+++ server/middlewares/passport-local.js
@@ -0,0 +1,40 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/User');
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: 'email', passReqToCallback: true },
+    async (req, username, password, done) => {
+      try {
+        const user = await User.findOne({ email: username }).exec();
+        if (!user) {
+          return done(null, false, { message: 'Invalid username or password' });
+        }
+        const isValidPassword = await user.validatePassword(password);
+        if (!isValidPassword) {
+          return done(null, false, { message: 'Invalid username or password' });
+        }
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    },
+  ),
+);
+
+passport.serializeUser((user, done) => done(null, user._id));
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id).exec();
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+});
+
+module.exports = {
+  initialize: passport.initialize(),
+  session: passport.session(),
+};
